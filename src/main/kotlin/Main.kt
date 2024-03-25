da34739362a6e2952a3b50ba0574215c8851addb
Raziah <abdullahiraziah@gmail.com>
2024-03-25 08:25:39 +0300

    added assessment2

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main() {
+    calculations(3,4,5)
+    vol(3.14159,21)
+    ispalindrome("kayak")
+
+}
+
+fun extract(ArrayOf:String){
+    var arr = "Barnie bakes brown bagels and buns"
+    var q = " "
+
+    println()
+
+
+
+}
+fun calculations(num1:Int,num2:Int,num3:Int):Int{
+    var sum = num1 + num2 + num3
+    var average = sum / 3
+    return sum
+    return average
+
+
+
+
+}
+fun vol(Double:Double ,numb:Int){
+    var product = Double * numb
+    println(product)
+
+}
+fun ispalindrome(word:String):Boolean{
+    var name = "madam"
+    return true
+
+}
+
